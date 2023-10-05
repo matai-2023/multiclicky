64dd68323852bea065508e113a1969c2068aa14c
Yuekai Wu <yuekaixx@outlook.com>
2023-10-05 16:58:05 +1300

    set up socket

--- client/components/App.tsx
+++ client/components/App.tsx
@@ -1,5 +1,16 @@
 import AnimatedRoutes from './AnimatedRoutes'
 
+import { io } from 'socket.io-client'
+const socket = io('http://localhost:3000', { transports: ['websocket'] })
+
+socket.on('connect', () => {
+  console.log(socket.id)
+})
+
+socket.on('disconnect', () => {
+  console.log('Disconnected')
+})
+
 function App() {
   return (
     <>
--- server/index.ts
+++ server/index.ts
@@ -1,9 +1,34 @@
-import server from './server.ts'
-import dotenv from 'dotenv'
+import express from 'express'
+import * as Path from 'node:path'
 
-dotenv.config()
-const port = process.env.PORT || 3000
+const server = express()
 
-server.listen(port, () => {
-  console.log('Server listening on port', port)
+import { createServer } from 'http'
+import { Server } from 'socket.io'
+
+const app = express()
+const httpServer = createServer(app)
+const io = new Server(httpServer, {})
+
+httpServer.listen(3000)
+
+server.use(express.json())
+
+server.use(express.static(Path.resolve('public')))
+
+if (process.env.NODE_ENV === 'production') {
+  server.use('/assets', express.static(Path.resolve('./dist/assets')))
+  server.get('*', (req, res) => {
+    res.sendFile(Path.resolve('./dist/index.html'))
+  })
+}
+
+io.on('connection', (socket) => {
+  console.log(socket.id) // x8WIv7-mJelg7on_ALbx
 })
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+})
+
+export default server
